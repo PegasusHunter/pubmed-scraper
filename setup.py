12948c0e608e647687b80ed8f409aea1c96bed4c
PegasusHunter <9reedy@example.com>
2022-01-27 04:28:33 +0300

    fix setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
+
 
 setup(
     name='pubmed-scraper',
@@ -13,6 +14,7 @@ setup(
     description='PubMed scraper',
     long_description=long_description,
     author='Alexey Severin',
+    packages=find_packages(),
     install_requires=requirements,
     keywords=['pubmed', 'scraper'],
 )
